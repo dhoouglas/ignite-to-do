05f6b8ba63b95a024d39dffd9507f9cb42a637b7
unknown <dhoouglas@hotmail.com>
2023-06-06 00:45:20 -0300

    Created  component Tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Header } from './components/Header'
+import { Tasks } from './components/Tasks'
 import './styles/global.css'
 
 function App() {
@@ -6,6 +7,8 @@ function App() {
   return (
     <>
       <Header />
+
+      <Tasks />
     </>
   )
 }
